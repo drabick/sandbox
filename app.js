b2b82f031edb89034688b3268dbf1b4e45a2f09a
drabick <drabick@hawk.iit.edu>
2020-02-08 16:04:41 -0600

    Added mongoose env and set first mongo db connection

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv/config');
+
+//middleware
+
+//app.use('/posts', () => {
+//    console.log('THis is some middleware running');
+//})
+
+
 
 //routes
 app.get('/', (req,res) => {
     res.send('We are on home page');    
 });
+
+app.get('/posts', (req,res) => {
+    res.send('We are on post page');    
+});
+
+//Conect to MongoDB
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    //'mongodb://drabick:Tron1414!@ds035448.mlab.com:35448/mcp13',
+    { useNewUrlParser: true, useUnifiedTopology: true }, 
+    () => 
+    console.log('Connected to MongoDB')
+);
+
+
 // lisgerner
 
 app.listen(3000);
