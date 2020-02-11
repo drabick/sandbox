acd52388a6e319869e22ec64f3fab77adc66793c
drabick <drabick@hawk.iit.edu>
2020-02-10 21:18:17 -0600

    Added API ID data to Handlebar test

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const handlebars = require('express-handlebars');
+const fetch = require('node-fetch');
+
 const app = express();
 require('dotenv/config');
 
@@ -18,7 +20,7 @@ app.use('/posts', postsRoute);
 //Handlebar engine
 //Sets handlebars configurations (we will go through them later on)
 app.engine('handlebars', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
+    layoutsDir: __dirname + '/views/layouts',defaultLayout: 'index',
     }));
 app.set('view engine', 'handlebars');
 
@@ -26,11 +28,31 @@ app.set('view engine', 'handlebars');
 //app.get('/', (req,res) => {
 //    res.send('We are on home page');    
 //});
+var cdata;
+
+fetch(' https://data.cityofchicago.org/resource/crimes.json')
+.then(response => {
+    return response.json()
+})
+.then(data => {
+//Work with JSON data here
+console.log(data[0].id) // Grab first Crime ID from first JSON element in Crime array
+cdata = data;
+})
+.catch(err=> {
+// Do something for an error here
+})
 
 //load index layout and fill it with main html data into body
 app.get('/', (req, res) => {
+    
+
+
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', {layout : 'index'});
+    //res.render('main', {layout : 'index'});
+    
+    res.render('main', {crimes: cdata});
+
     });
 
 //Conect to MongoDB
